f9eca1026511394c82e8e448d7f79d2346330409
Chelsea Metcalf <chelseametcalf@ufl.edu>
2015-11-18 02:17:05 -0500

    Input and return UserEnt. Need to make function more general. :baby_chick:

--- fbclient/src/main/scala/FBClient.scala
+++ fbclient/src/main/scala/FBClient.scala
@@ -6,7 +6,11 @@ object FBClient {
     val system = ActorSystem("Facebook-System")
     val master = system.actorOf(Props(new Master()), "master")
     master ! CreateUsers
-    val Network = new Network()
+    //val Network = new Network()
+    //Network.addUser()
     //println("Hello from Facebook Client!")
+    /*Network.addUser("Chelsea", "Metcalf", DateTime.now, Gender.Female,
+      "chelsea.metcalf@gmail.com", "Test about", RelationshipStatus.Single,
+      Gender.Male, PoliticalAffiliation.Democrat, TimeZone.getDefault)*/
   }
 }
