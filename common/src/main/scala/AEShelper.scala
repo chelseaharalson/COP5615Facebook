117134c560425a7455c63eb941209f98f0279a41
Chelsea Metcalf <chelseametcalf@ufl.edu>
2015-12-11 15:55:12 -0500

    Removed out unncessary stuff.

--- common/src/main/scala/AEShelper.scala
+++ common/src/main/scala/AEShelper.scala
@@ -4,8 +4,6 @@ import java.nio.ByteBuffer
 import java.security.SecureRandom
 import java.util.Arrays
 
-//case class AESobj(key : SecretKey, nonce : Array[Byte])
-
 class AEShelper {
 
   val AES_KEY_SIZE = 128
@@ -13,9 +11,7 @@ class AEShelper {
   val GCM_TAG_LENGTH = 16
 
   val cipher = Cipher.getInstance("AES/GCM/NoPadding", "SunJCE")
-  //val nonce = new Array[Byte](GCM_NONCE_LENGTH)
   val random = SecureRandom.getInstanceStrong()
-  //val spec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, nonce)
 
   def generateKey() : (SecretKey, Array[Byte]) = {
     val keyGen = KeyGenerator.getInstance("AES")
@@ -30,7 +26,7 @@ class AEShelper {
     val spec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, nonce)
     cipher.init(Cipher.ENCRYPT_MODE, key, spec)
     val cipherText = cipher.doFinal(input.getBytes())
-    println("CIPHER TEXT: " + cipherText)
+    //println("CIPHER TEXT: " + cipherText)
     cipherText
   }
 
@@ -38,7 +34,7 @@ class AEShelper {
     val spec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, nonce)
     cipher.init(Cipher.DECRYPT_MODE, key, spec)
     val plainText = cipher.doFinal(cipherText)
-    println("PLAIN TEXT: " + plainText)
+    //println("PLAIN TEXT: " + plainText)
     new String(plainText)
   }
 }
