b598523bc9af22494dd9d1d58cd8ca62dcf0cf3d
Chelsea Metcalf <chelseametcalf@ufl.edu>
2015-12-17 00:05:06 -0500

    Started skeleton of FacebookACL. canAccess function checks id in lists.

--- common/src/main/scala/FacebookACL.scala
+++ common/src/main/scala/FacebookACL.scala
@@ -1,6 +1,38 @@
-/**
- * Created by chelsea on 12/16/15.
- */
-class FacebookACL {
+import scala.collection.mutable
+import util.control.Breaks._
 
+class FacebookACL() {
+
+  def canAccess(ownerId : Identifier, privacy : Privacy.EnumVal, userMap : mutable.HashMap[Identifier, UserEnt],
+                friendList : mutable.HashMap[Identifier, FriendsList]) : Boolean = {
+
+    var accessGranted = false
+
+    if (privacy == Privacy.OnlyMe) {
+      breakable {
+        for ((userId, userEntInfo) <- userMap) {
+          if (userId == ownerId) {
+            accessGranted = true
+            break
+          }
+          else if (userId != ownerId) {
+            accessGranted = false
+          }
+        }
+      }
+    }
+    else if (privacy == Privacy.Friends) {
+      if (friendList.contains(ownerId)) {
+        accessGranted = true
+      }
+      else {
+        println("User denied permission because not friend!")
+        accessGranted = false
+      }
+    }
+    else if (privacy == Privacy.Public) {
+      accessGranted = true
+    }
+    accessGranted
+  }
 }
